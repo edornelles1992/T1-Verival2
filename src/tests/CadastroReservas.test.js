c734f57e0fc984140970f25d8163a7bac2bf49f2
João Lucas de Almeida <joaolucas.alm@gmail.com>
2020-06-06 20:21:54 -0300

    teste cadastro de reserva

--- src/tests/CadastroReservas.test.js
+++ src/tests/CadastroReservas.test.js
@@ -2,7 +2,8 @@ import CadastroReservas from '../views/CadastroReservas'
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
 import React from 'react';
-import { reservaValida } from './MockObjects'
+import { reservaDataValida } from './MockObjects'
+import { reservaDataInvalidaMesmoDia } from './MockObjects'
 
 configure({adapter: new Adapter()});
 
@@ -11,19 +12,20 @@ test('Deve instanciar a Page Cadastro Reserva',() => {
     expect(cadastroReservas).toBeDefined();
 })
 
-test('Deve cadastrar uma nova reserva', async () => {
-    const wrapper = shallow(<CadastroReservas/>);
-    wrapper.instance().cadastrarReserva()
-})
-
-test('Deve validar uma reserva', async () => {
+test('Deve validar uma reserva com data válida', () => {
     const wrapper = shallow(<CadastroReservas/>);
     let instance = wrapper.instance()
 
-    instance.state.reserva = reservaValida
+    instance.state.reserva = reservaDataValida
     instance.validarReserva()
     expect(instance.validarReserva()).toBe(true);
 })
 
-
+test('Deve validar uma reserva com data inválida', () => {
+    const wrapper = shallow(<CadastroReservas/>);
+    let instance = wrapper.instance()
+    instance.state.reserva = reservaDataInvalidaMesmoDia
+    instance.validarReserva()
+    expect(instance.validarReserva()).toBe(false);
+})
 
--- src/tests/MockObjects.js
+++ src/tests/MockObjects.js
@@ -1,7 +1,22 @@
-export const reservaValida = { 
+export const reservaDataValida = { 
     dataInicio: "2020-10-25", 
     dataFim: "2020-10-27", 
     nome: "Airton", 
+    idUsuario: 1, 
+    custo: 200, 
+    recurso: {
+        id: "3",
+        nome: "Auditório 32",
+        tipo: "espaço",
+        tamanho: 90,
+        assentos: 120
+    },
+}
+
+export const reservaDataInvalidaMesmoDia = { 
+    dataInicio: "2020-10-10", 
+    dataFim: "2020-10-10", 
+    nome: "João", 
     idUsuario: 1, 
     custo: 200, 
     recurso: {
