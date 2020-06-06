b1bde9eca27d6146fb2fbd6bf99d6187dda619db
João Lucas de Almeida <joaolucas.alm@gmail.com>
2020-06-06 19:49:38 -0300

    testes cadastro reserva valida

--- src/tests/CadastroReservas.test.js
+++ src/tests/CadastroReservas.test.js
@@ -1,8 +1,29 @@
 import CadastroReservas from '../views/CadastroReservas'
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
+import React from 'react';
+import { reservaValida } from './MockObjects'
+
+configure({adapter: new Adapter()});
 
 test('Deve instanciar a Page Cadastro Reserva',() => {
     let cadastroReservas = new CadastroReservas();
     expect(cadastroReservas).toBeDefined();
 })
+
+test('Deve cadastrar uma nova reserva', async () => {
+    const wrapper = shallow(<CadastroReservas/>);
+    wrapper.instance().cadastrarReserva()
+})
+
+test('Deve validar uma reserva', async () => {
+    const wrapper = shallow(<CadastroReservas/>);
+    let instance = wrapper.instance()
+
+    instance.state.reserva = reservaValida
+    instance.validarReserva()
+    expect(instance.validarReserva()).toBe(true);
+})
+
 
 
---
+++ src/tests/MockObjects.js
@@ -0,0 +1,14 @@
+export const reservaValida = { 
+    dataInicio: "2020-10-25", 
+    dataFim: "2020-10-27", 
+    nome: "Airton", 
+    idUsuario: 1, 
+    custo: 200, 
+    recurso: {
+        id: "3",
+        nome: "Auditório 32",
+        tipo: "espaço",
+        tamanho: 90,
+        assentos: 120
+    },
+}
